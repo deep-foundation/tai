1437536c36aca39428404e93ad44d558e9509ec5
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-07 22:22:53 +0000

    add voice-recorder into imports

---
+++ imports/capacitor-voice-recorder/upload-records.ts
@@ -0,0 +1,84 @@
+import { PACKAGE_NAME } from "./install-package";
+
+export default async function uploadRecords(deep, deviceLinkId, sounds) {
+  const containTypeLinkId = await deep.id("@deep-foundation/core", "Contain");
+  const audioRecordsLinkId = await deep.id(PACKAGE_NAME, "AudioRecords");
+  const soundTypeLinkId = await deep.id("@deep-foundation/sound", "Sound");
+  const recordTypeLinkId = await deep.id(PACKAGE_NAME, "Record");
+  const durationTypeLinkId = await deep.id(PACKAGE_NAME, "Duration");
+  const startTimeTypeLinkId = await deep.id(PACKAGE_NAME, "StartTime");
+  const endTimeTypeLinkId = await deep.id(PACKAGE_NAME, "EndTime");
+  const mimetypeTypeLinkId = await deep.id("@deep-foundation/sound", "MIME/type");
+  const formatTypeLinkId = await deep.id("@deep-foundation/sound", "Format");
+
+  await deep.insert(sounds.map((sound) => ({
+    type_id: recordTypeLinkId,
+    in: {
+      data: [{
+        type_id: containTypeLinkId,
+        from_id: audioRecordsLinkId,
+      }]
+    },
+    out: {
+      data: [
+        {
+          type_id: containTypeLinkId,
+          to: {
+            data: {
+              type_id: soundTypeLinkId,
+              string: { data: { value: sound.record ? sound.record["recordDataBase64"] : '' } },
+              out: {
+                data: [
+                  {
+                    type_id: containTypeLinkId,
+                    to: {
+                      data: {
+                        type_id: mimetypeTypeLinkId,
+                        string: { data: { value: sound.record ? sound.record["mimeType"] : '' } },
+                      }
+                    }
+                  },
+                  {
+                    type_id: containTypeLinkId,
+                    to: {
+                      data: {
+                        type_id: formatTypeLinkId,
+                        string: { data: { value: sound.record && sound.record["mimeType"] === "audio/webm;codecs=opus" ? "webm" : "aac" } },
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        },
+        {
+          type_id: containTypeLinkId,
+          to: {
+            data: {
+              type_id: durationTypeLinkId,
+              number: { data: { value: sound.record ? sound.record["msDuration"] : null } },
+            }
+          }
+        },
+        {
+          type_id: containTypeLinkId,
+          to: {
+            data: {
+              type_id: startTimeTypeLinkId,
+              string: { data: { value: sound.startTime } },
+            }
+          }
+        },
+        {
+          type_id: containTypeLinkId,
+          to: {
+            data: {
+              type_id: endTimeTypeLinkId,
+              string: { data: { value: sound.endTime } },
+            }
+          }
+        }]
+    }
+  })));
+}
