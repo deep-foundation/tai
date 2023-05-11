a2d3e3d626a7ab2bff24f50dce777b8bed1618fc
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-11 11:18:31 +0000

    add apikey and google auth fields to providers-and-login-or-content file

---
+++ components/providers-and-login-or-content.tsx
@@ -0,0 +1,51 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { DeepProvider } from "@deep-foundation/deeplinks/imports/client";
+import { TokenProvider } from "@deep-foundation/deeplinks/imports/react-token";
+import { useLocalStore } from "@deep-foundation/store/local";
+import { CapacitorStoreKeys } from "../imports/capacitor-store-keys";
+import { LoginOrContent } from "./login-or-content";
+import { ApolloClientTokenizedProvider } from '@deep-foundation/react-hasura/apollo-client-tokenized-provider';
+
+export function ProvidersAndLoginOrContent({ children }: { children: JSX.Element }) {
+  const [gqlPath, setGqlPath] = useLocalStore(CapacitorStoreKeys[CapacitorStoreKeys.GraphQlPath], undefined)
+  const [apiKey, setApiKey] = useLocalStore("apikey", undefined);
+  const [googleAuth, setGoogleAuth] = useLocalStore("googleAuth", undefined);
+
+  return (
+    <>
+      <ChakraProvider>
+        <TokenProvider>
+          <ApolloClientTokenizedProvider
+            options={{
+              client: 'deeplinks-app',
+              path: gqlPath,
+              ssl: true,
+              ws: !!process?.browser,
+            }}
+          >
+            <DeepProvider>
+              <LoginOrContent 
+                gqlPath={gqlPath} setGqlPath={(newGqlPath) => {
+                  console.log({ newGqlPath })
+                  setGqlPath(newGqlPath)
+                }}
+                apiKey={apiKey}
+                setApiKey={(newApiKey) => {
+                  console.log({ newApiKey })
+                  setApiKey(newApiKey)
+                }}
+                googleAuth={googleAuth}
+                setGoogleAuth={(newGoogleAuth) => {
+                  console.log({ newGoogleAuth })
+                  setGoogleAuth(newGoogleAuth)
+                }}
+              >
+                {children}
+              </LoginOrContent>
+            </DeepProvider>
+          </ApolloClientTokenizedProvider>
+        </TokenProvider>
+      </ChakraProvider>
+    </>
+  );
+}
