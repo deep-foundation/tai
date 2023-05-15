3c6a8d83e1f24d6f202969fcad2cef55423dbb90
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-15 13:16:40 +0000

    delete type for start-recording

--- imports/capacitor-voice-recorder/strart-recording.ts
+++ imports/capacitor-voice-recorder/strart-recording.ts
@@ -1,6 +1,6 @@
 import { VoiceRecorder } from "capacitor-voice-recorder";
 
-export default async function startRecording(): Promise<string> {
+export default async function startRecording() {
     const { value: isrecording } = await VoiceRecorder.startRecording();
     const startTime = new Date().toLocaleDateString();
     return startTime;
