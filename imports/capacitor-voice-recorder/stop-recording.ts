9f5219567dc393d08e2958a697c9ca88b5c7fc2c
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-15 23:23:48 +0000

    delete upload from stop-recording

--- imports/capacitor-voice-recorder/stop-recording.ts
+++ imports/capacitor-voice-recorder/stop-recording.ts
@@ -4,7 +4,6 @@ import uploadRecords from "./upload-records";
 
 export default async function stopRecording(deep: DeepClient, containerLinkId, startTime) {
     const { value: record } = await VoiceRecorder.stopRecording();
-    const endTime = new Date().toLocaleDateString();
-    await uploadRecords(deep, containerLinkId, [{record, startTime, endTime}])
+    console.log("record",record)
     return record;
 }
