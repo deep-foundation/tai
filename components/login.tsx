8182e58a77adf3ab4630cab3526d15b6a6d0e18c
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-26 15:44:53 +0000

    delete WithPackagesInstalled from the login component

--- components/login.tsx
+++ components/login.tsx
@@ -18,60 +18,6 @@ export function Setup(arg: {
   const [arePermissionsGranted, setArePermissionsGranted] = useState<boolean>(false)
   const [packagesBeingInstalled, setPackagesBeingInstalled] = useState<Array<string>>([]);
   const [packagesInstalled, setPackagesInstalled] = useState<Array<string>>([]);
-  
-  const installPackage = async (packageName) => {
-    if (!packagesBeingInstalled[packageName]) {
-      await deep.insert([
-        {
-          type_id: await deep.id('@deep-foundation/npm-packager', 'Install'),
-          from_id: deep.linkId,
-          to: {
-            data: {
-              type_id: await deep.id('@deep-foundation/core', 'PackageQuery'),
-              string: { data: { value: packageName } }
-            }
-          },
-        }
-      ]);
-      let packageLinkId;
-      while (!packageLinkId) {
-        try {
-          packageLinkId = await deep.id(packageName);
-        } catch (error) {
-          console.log(`Package ${packageName} not installed yet, retrying in 1 second...`);
-          await new Promise(resolve => setTimeout(resolve, 1000));
-        }
-      }
-
-      await deep.insert([
-        {
-          type_id: await deep.id("@deep-foundation/core", "Join"),
-          from_id: packageLinkId,
-          to_id: await deep.id('deep', 'users', 'packages'),
-        },
-        {
-          type_id: await deep.id("@deep-foundation/core", "Join"),
-          from_id: packageLinkId,
-          to_id: await deep.id('deep', 'admin'),
-        },
-      ]);
-
-      if(packageName==="@deep-foundation/chatgpt"){
-        await deep.insert([
-          {
-            type_id: await deep.id("@deep-foundation/core", "Join"),
-            from_id: await deep.id("@deep-foundation/chatgpt-tokens-gpt-3-encoder"),
-            to_id: await deep.id('deep', 'users', 'packages'),
-          },
-          {
-            type_id: await deep.id("@deep-foundation/core", "Join"),
-            from_id: await deep.id("@deep-foundation/chatgpt-tokens-gpt-3-encoder"),
-            to_id: await deep.id('deep', 'admin'),
-          },
-        ]);
-      }
-    }
-  };
 
   const submitForm = async () => {
     arg.onSubmit({
@@ -160,54 +106,6 @@ export function Setup(arg: {
           }}>
             Submit
           </Button>
-          {isSendDataPressed && (
-            <WithPackagesInstalled
-              packageNames={["@deep-foundation/capacitor-voice-recorder", "@deep-foundation/google-speech", "@deep-foundation/chatgpt"]}
-              renderIfError={(error) => <div>{error.message}</div>}
-              renderIfNotInstalled={(packageNames) => {
-                return (
-                  <div>
-                    {`Install these deep packages to proceed: ${packageNames.join(', ')}`},
-                    {
-                      packageNames
-                      .filter((packageName) => !packagesBeingInstalled.includes(packageName))
-                      .map((packageName) => {
-                        if (packagesInstalled.includes(packageName)) {
-                          return null;
-                        }
-                        return (
-                          <Button onClick={() => {
-                            installPackage(packageName);
-                            setPackagesInstalled([...packagesInstalled, packageName]);
-                          }}>
-                            Install {packageName}
-                          </Button>
-                        );
-                    })
-                  }
-                  </div>
-                );
-              }}
-              renderIfLoading={() => (
-                <Text>Checking if deep packages are installed...</Text>
-              )}
-              shouldIgnoreResultWhenLoading={true}
-            >
-              {!arePermissionsGranted ? (
-                <>
-                  <Button
-                    style={{ position: 'relative', zIndex: 1000 }}
-                    onClick={async () => {
-                      const { value: arePermissionsGranted } = await VoiceRecorder.requestAudioRecordingPermission();
-                      setArePermissionsGranted(arePermissionsGranted);
-                    }}
-                  >
-                    GET RECORDING PERMISSION
-                  </Button>
-                </>
-              ) : <></>}
-            </WithPackagesInstalled>
-          )}
         </CardBody>
       </Card>
     </>
