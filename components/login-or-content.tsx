55d41e67bb346fff96ec692a438207cdf85589ca
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-28 20:45:51 +0000

    delete sendData button from the login-or-content

--- components/login-or-content.tsx
+++ components/login-or-content.tsx
@@ -5,7 +5,6 @@ import { Setup } from "./login";
 export function LoginOrContent({ gqlPath, setGqlPath, children, apiKey, googleAuth, setApiKey, setGoogleAuth, systemMsg, setSystemMsg }: { gqlPath: string | undefined, setGqlPath: (gqlPath: string | undefined) => void, children: JSX.Element, apiKey: string | undefined, setApiKey: (apiKey: string | undefined) => void, googleAuth: string | undefined, setGoogleAuth: (googleAuth: string | undefined) => void, systemMsg: string | undefined, setSystemMsg: (systemMsg: string | undefined) => void }) {
   const deep = useDeep();
   const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
-  const [isSubmitted, setIsSubmitted] = useState(false);
 
   useEffect(() => {
     self["deep"] = deep
@@ -16,29 +15,18 @@ export function LoginOrContent({ gqlPath, setGqlPath, children, apiKey, googleAu
     }
   }, [deep]);
 
-  useEffect(() => {
-    const isSubmitted = localStorage.getItem('isSubmitted');
-    if (isSubmitted === 'true') {
-      setIsSubmitted(true);
-    }
-  }, []);
-
   console.log({ isAuthorized, gqlPath })
-  return isAuthorized && gqlPath && googleAuth && apiKey && systemMsg && isSubmitted ? children : (
+  return isAuthorized && gqlPath && googleAuth && apiKey && systemMsg ? children : (
     <Setup
-      onAuthorize={(arg) => {
+      onSubmit={(arg) => {
         console.log({ arg })
         setGqlPath(arg.gqlPath);
         deep.login({
           token: arg.token
         })
-      }}
-      onSubmit={(arg) => {
         setApiKey(arg.apiKey);
         setGoogleAuth(arg.googleAuth);
         setSystemMsg(arg.systemMsg);
-        localStorage.setItem('isSubmitted', 'true');
-        setIsSubmitted(true);
       }}
     />
   );
