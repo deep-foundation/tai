a3b3ac3ede1f0100399b5b661b6d6d7fd6839fc9
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-01 13:31:08 +0600

    Upgrade code

---
+++ components/with-setup.tsx
@@ -0,0 +1,57 @@
+import { DeepClient, useDeep } from '@deep-foundation/deeplinks/imports/client';
+import { useState, useEffect } from 'react';
+import { Setup } from './login';
+
+export function WithSetup({
+  renderChildren,
+  gqlPath,
+  setGqlPath,
+  apiKey,
+  googleAuth,
+  setApiKey,
+  setGoogleAuth,
+  systemMsg,
+  setSystemMsg,
+}: LoginOrContentParam) {
+  const deep = useDeep();
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+
+  useEffect(() => {
+    self['deep'] = deep;
+    if (deep.linkId !== 0) {
+      setIsAuthorized(true);
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [deep]);
+
+  console.log({ isAuthorized, gqlPath });
+  return isAuthorized && gqlPath && googleAuth && apiKey && systemMsg ? (
+    renderChildren({deep})
+  ) : (
+    <Setup
+      onSubmit={(arg) => {
+        console.log({ arg });
+        setGqlPath(arg.gqlPath);
+        deep.login({
+          token: arg.token,
+        });
+        setApiKey(arg.apiKey);
+        setGoogleAuth(arg.googleAuth);
+        setSystemMsg(arg.systemMsg);
+      }}
+    />
+  );
+}
+
+export interface LoginOrContentParam {
+  gqlPath: string | undefined;
+  setGqlPath: (gqlPath: string | undefined) => void;
+  apiKey: string | undefined;
+  setApiKey: (apiKey: string | undefined) => void;
+  googleAuth: string | undefined;
+  setGoogleAuth: (googleAuth: string | undefined) => void;
+  systemMsg: string | undefined;
+  setSystemMsg: (systemMsg: string | undefined) => void;
+  renderChildren: (param: { deep: DeepClient }) => JSX.Element;
+}
