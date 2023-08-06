87bf0adadcc2a7f224c79f2cf38d1ec0a1ccf034
flakeed <flakeeed@gmail.com>
2023-08-06 00:23:21 +0000

    Update Items modal window logic

--- components/items-modal.tsx
+++ components/items-modal.tsx
@@ -1,26 +1,35 @@
 // components/ItemsModal.js
-
-import React from 'react';
+// import qr code
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const ItemsModal = ({ isOpen, onRequestClose, items }) => {
+    const [showQRCode, setShowQRCode] = useState(false);
+    const [qrCodeValue, setQRCodeValue] = useState('');
+
+    const handleBuy = () => {
+        setQRCodeValue('newconversationlink');
+        setShowQRCode(true);
+    };
+
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
             contentLabel="Items"
         >
-            {items.map((item, index) => (
-                <div key={index}>
-                    <img src={item.image} alt={item.name} />
-                    <h2>{item.name}</h2>
-                    <p>{item.description}</p>
-                    <p>{item.price}</p>
-                    <button>Add to Cart</button>
-                </div>
-            ))}
-            <button onClick={onRequestClose}>Close</button>
-            <button style={{ position: 'absolute', right: 0, bottom: 0 }}>Buy</button>
+            {/* {showQRCode && <QRCode value={qrCodeValue} />} */}
+
+{items.map((item) => (
+  <div key={item.id}>
+    <img src={item.imageUrl} alt={item.itemName} width="100" height="100" />
+    <h2>{item.itemName}</h2>
+    <p dangerouslySetInnerHTML={{ __html: item.description }} />
+    <p>Price: {item.price || 'N/A'}</p>
+    <button onClick={handleBuy}>Add to Cart</button>
+  </div>
+))}
+            <button onClick={onRequestClose} style={{ position: 'absolute', right: 0, bottom: 0 }}>Buy</button>
         </Modal>
     );
 };
