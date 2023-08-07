c296662ec93abb4b16d0a5901f1c443f7d991596
flakeed <flakeeed@gmail.com>
2023-08-07 00:58:14 +0000

    Add cart for each item to items-modal

--- components/items-modal.tsx
+++ components/items-modal.tsx
@@ -44,7 +44,18 @@ overflowY: 'auto',
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
   transition: '0.3s'
 }}>
-
+<img 
+src={item.imageUrl || 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><rect width="100" height="100" fill="black"/><text x="50" y="60" font-family="Arial" font-size="40" text-anchor="middle" fill="white">?</text></svg>'} 
+alt={item.itemName} 
+width="100" 
+height="100" 
+style={{ borderRadius: '10px' }} 
+/>
+  <div style={{ flex: 1, marginLeft: '15px' }}>
+    <h2 style={{ fontSize: '1.4rem', color: '#388E3C', marginBottom: '5px' }}>{item.itemName}</h2>
+    <p style={{ fontSize: '1.1rem', color: '#2E7D32', fontWeight: 'bold' }}>Price: {item.price || 'N/A'}</p>
+  </div>
+  
     </div>
   )}
 
