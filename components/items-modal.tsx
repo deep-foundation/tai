b29734dc06eca6dc1a1217d05d4ee364be9fedf9
flakeed <flakeeed@gmail.com>
2023-08-07 00:48:28 +0000

    Update Items-model structure

--- components/items-modal.tsx
+++ components/items-modal.tsx
@@ -4,34 +4,7 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 
 const ItemsModal = ({ isOpen, onRequestClose, items }) => {
-    const [showQRCode, setShowQRCode] = useState(false);
-    const [qrCodeValue, setQRCodeValue] = useState('');
 
-    const handleBuy = () => {
-        setQRCodeValue('newconversationlink');
-        setShowQRCode(true);
-    };
-
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            contentLabel="Items"
-        >
-            {/* {showQRCode && <QRCode value={qrCodeValue} />} */}
-
-{items.map((item) => (
-  <div key={item.id}>
-    <img src={item.imageUrl} alt={item.itemName} width="100" height="100" />
-    <h2>{item.itemName}</h2>
-    <p dangerouslySetInnerHTML={{ __html: item.description }} />
-    <p>Price: {item.price || 'N/A'}</p>
-    <button onClick={handleBuy}>Add to Cart</button>
-  </div>
-))}
-            <button onClick={onRequestClose} style={{ position: 'absolute', right: 0, bottom: 0 }}>Buy</button>
-        </Modal>
-    );
 };
 
 export default ItemsModal;
